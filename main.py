3315e27402524858619dd2aff98179d9b9f04b5b
Yekong995 <yekongyt@gmail.com>
2023-11-15 22:09:21 +0800

    Add static folder

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ console = Console()
 trace_install(console=console)
 pretty_install(console=console)
 
-app = Flask(__name__, template_folder="templates")
+app = Flask(__name__, template_folder="templates", static_folder="static")
 app.secret_key = os.urandom(24)
 csp = ContentSecurityPolicy()
 hsts = StrictTransportSecurity()
